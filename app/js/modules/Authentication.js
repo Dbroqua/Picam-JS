e1dcfc5f510adbe41ae97429a8c36e65c92fa329
dbroqua <dbroqua@mousur.org>
2016-08-21 22:01:50 +0200

    IMPROVEMENT: Updated login form

--- app/js/modules/Authentication.js
+++ app/js/modules/Authentication.js
@@ -8,6 +8,7 @@ angular.module('Authentication')
             'use strict';
             $rootScope.title = TITLEPrefix + 'Login';
             $rootScope.bodyClass = 'signin';
+            $scope.message = null;
 
             $scope.$on('$destroy',function(){
                 $rootScope.bodyClass = '';
@@ -15,13 +16,9 @@ angular.module('Authentication')
 
             $scope.username = '';
             $scope.password = '';
-            $scope.message = {
-                'class': 'alert-danger',
-                'message': null
-            };
 
             $scope.login = function () {
-                $scope.message.message = null;
+                $scope.message = null;
                 if ($scope.username !== '' && $scope.password !== '') {
                     HTTPService.login($scope.username, $scope.password,
                         function () {
@@ -30,18 +27,18 @@ angular.module('Authentication')
                             if (error !== null && error.status !== undefined) {
                                 switch (error.status) {
                                     case 401:
-                                        $scope.message.message = '<strong>Error !</strong> Bad logins';
+                                        $scope.message = 'Bad login';
                                         break;
                                     default:
-                                        $scope.message.message = '<strong>Error !</strong> Unknown error';
+                                        $scope.message = 'Unknown error';
                                 }
                             } else {
-                                $scope.message.message = '<strong>Error !</strong> Server downn ?';
+                                $scope.message = 'Server downn ?';
                             }
                         });
                 }
                 else {
-                    $scope.message.message = '<strong>Error !</strong> Malformated form';
+                    $scope.message = '<strong>Error !</strong> Malformated form';
                 }
             };
         }
