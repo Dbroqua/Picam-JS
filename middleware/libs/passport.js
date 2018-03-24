c3d4662a292409e5b5cabf2545fad6789a899c64
dbroqua <dbroqua@mousur.org>
2018-03-24 14:45:50 +0100

    Allow enable/disable user

--- middleware/libs/passport.js
+++ middleware/libs/passport.js
@@ -6,33 +6,43 @@ let BasicAuth = require('passport-http').BasicStrategy, //Basic Auth
     ApiStrategy = require('passport-localapikey').Strategy, //Api Key
     users = require('../../models/v1/users');
 
-module.exports = function (passport) {
+module.exports = function(passport) {
     passport.use('basic', new BasicAuth({
             //By default, local strategy uses username and password, we will override with email
             usernameField: 'mail',
             passwordField: 'password',
             passReqToCallback: true //Allows us to pass back the entire request to the callback
         },
-        function (req, mail, password, done) {
-            users.model.findOne({mail: mail}, function (err, user) {
+        function(req, mail, password, done) {
+            users.model.findOne({
+                mail: mail
+            }, function(err, user) {
                 if (err) {
-                    return done(err, false, {message: 'Invalid username'});
+                    return done(err, false, {
+                        message: 'Invalid username'
+                    });
                 } else {
                     //Test a matching password
                     if (user !== null) {
-                        user.comparePassword(password, function (err, isMatch) {
+                        user.comparePassword(password, function(err, isMatch) {
                             if (err) {
-                                return done(err, false, {message: 'Invalid password'});
+                                return done(err, false, {
+                                    message: 'Invalid password'
+                                });
                             } else {
-                                if (isMatch) {
+                                if (isMatch && user.active === true) {
                                     return done(null, user);
                                 } else {
-                                    return done(null, false, {message: 'Invalid password'});
+                                    return done(null, false, {
+                                        message: 'Invalid password'
+                                    });
                                 }
                             }
                         });
                     } else {
-                        return done(null, false, {message: 'Invalid username'});
+                        return done(null, false, {
+                            message: 'Invalid username'
+                        });
                     }
                 }
             });
@@ -42,14 +52,17 @@ module.exports = function (passport) {
             apiKeyField: 'apikey',
             passReqToCallback: true //Allows us to pass back the entire request to the callback
         },
-        function (req, apikey, done) {
-            users.model.findOne({ apikey: apikey }, function(err, user) {
-                if (err) {
-                    return done(err, false,{message: 'Invalid username'});
-                }else {
-                    return done(null, user );
+        function(req, apikey, done) {
+            users.model.findOne({
+                apikey: apikey
+            }, function(err, user) {
+                if (err || user.active === false) {
+                    return done(err, false, {
+                        message: 'Invalid username'
+                    });
+                } else {
+                    return done(null, user);
                 }
             });
-        })
-    );
+        }));
 };
